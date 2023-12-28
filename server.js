81fa4d22141ff41cf7553eb5bbfe864788b1d7a6
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2023-12-29 01:18:51 +0200

    connecting to database using mongoose

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
 const bootcampsRouter = require('./routes/bootcamps');
+
+dotenv.config({ path: './config/config.env' });
+const connectDB = require('./config/db');
+connectDB();
+
+const app = express();
+app.use(morgan('tiny'));
 
 app.use('/api/v1/bootcamps', bootcampsRouter);
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log(`the server is listening on port 3000`);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server
+  server.close(() => process.exit());
 });
